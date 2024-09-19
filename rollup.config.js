61d4502fd63362406340e9e4061e58910b1ff537
nmsbnmsb1 <nmsbnmsb1@126.com>
2024-09-19 20:43:59 +0700

    feat: add browser env support

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+const pkg = require('./package.json');
+const replace = require('@rollup/plugin-replace');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('rollup-plugin-typescript2');
+
+const node = {
+	input: './src/index.ts',
+	output: { file: pkg.main, format: 'cjs', sourcemap: true },
+	external: [...Object.keys(pkg.dependencies || {})],
+	plugins: [
+		resolve(),
+		commonjs(),
+		typescript({
+			typescript: require('typescript'),
+			abortOnError: true,
+			useTsconfigDeclarationDir: false,
+			tsconfigOverride: {
+				compilerOptions: { declarationDir: 'lib' },
+				include: ['src/**/*.ts'],
+			},
+		}),
+	],
+};
+const browser = {
+	input: './src/index.ts',
+	output: { format: 'esm', file: pkg.module, sourcemap: true },
+	external: [...Object.keys(pkg.dependencies || {})],
+	plugins: [
+		//replace({ './../node_modules/uuid/dist/esm-node/md5': `'blueimp-md5'`, delimiters: [`'`, `'`] }),
+		resolve(),
+		commonjs(),
+		typescript({
+			typescript: require('typescript'),
+			abortOnError: true,
+			useTsconfigDeclarationDir: false,
+			tsconfigOverride: {
+				compilerOptions: { declarationDir: 'lib/browser' },
+				include: ['src/**/*.ts'],
+			},
+		}),
+	],
+};
+
+module.exports = [node, browser];
